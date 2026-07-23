050f45991f9bd44abe6a40ea4cd1393fb9beb1d8
FGarcia012 <alexander.garcia.sicajau@gmail.com>
2026-07-23 13:53:00 -0600

    feat(server):servidor y base de datos colocados correctamente.

---
+++ configs/server.js
@@ -0,0 +1,45 @@
+'use strict'
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { dbconnection } from './mongo.js';
+import apiLimiter from '../src/middlewares/rate-limit-validator.js';
+
+const middlewares = (app) => {
+    app.use(express.urlencoded({ extended: false }))
+    app.use(express.json())
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan('dev'))
+    app.use(apiLimiter)
+}
+
+const router = (app) => {
+
+}
+
+const conectDB = async () => {
+    try {
+        await dbconnection()
+    }catch (err){
+        console.log(`Database connection failed: ${err}`)
+        process.exit(1)
+    }
+}
+
+export const initServer = async () => {
+    const app = express()
+    try{
+        middlewares(app)
+        conectDB()
+        router(app)
+        const port = process.env.PORT || 3010
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`)
+        })
+    }catch (err) {
+        console.log(`Server initialization failed: ${err}`)
+    }
+}
